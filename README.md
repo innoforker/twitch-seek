259f6c5c127a8c06597cd7bc43856269ed593cd4
Innoforker <innoforker@proton.me>
2025-02-01 09:39:46 +0300

    The base

---
+++ README.md
@@ -0,0 +1,5 @@
+# TwitchSeek
+Track all chat messages on Twitch
+**Features**:
+- The Twitch chats monitoring</li>
+- Don’t needed the authorization on Twitch</li>
---
+++ src/seeker.js
@@ -0,0 +1,21 @@
+// The Unlicense
+
+const prompt = require('prompt-sync')();
+const channel = prompt('Enter the name of the channel.\nKeep in mind that the live translation on this channel have to be started.\n\n');
+
+const tmi = require('tmi.js');
+const options = {
+    channels: [ channel ]
+};
+const client = new tmi.Client(options);
+
+console.log(`Monitoring ${options['channels'][0]}...`)
+client.on('message', (channel, userState, message, self) => {
+    if (self) return;
+
+    const timeStamp = new Date(parseInt(userState['tmi-sent-ts'])).toLocaleString();
+
+    console.log(`[${timeStamp}] ${userState.username}: ${message}`);
+});
+
+client.connect();
